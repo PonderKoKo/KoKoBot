471f0624bb0f913c161ef54bb1e6bbdba24e25e8
PonderKoKo <PonderKoKo+github@googlemail.com>
2021-08-30 10:02:13 +0200

    Already submitted decks are sorted

--- index.js
+++ index.js
@@ -83,6 +83,9 @@ client.once('ready', () => {
   })
   readFile(threeCardFileName, (content) => {
     module.exports.decks = JSON.parse(content)
+    for (const key in module.exports.decks) {
+      module.exports.decks[key].sort((a, b) => a.length - b.length)
+    }
     const [playerNames, playerIDs] = [{}, {}]
     module.exports.playerNames = playerNames
     module.exports.playerIDs = playerIDs
