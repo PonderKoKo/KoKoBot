3cb1152fd1e8c7e25bccca53ed2cd5f392faab2b
PonderKoKo <PonderKoKo+github@googlemail.com>
2021-09-06 19:07:44 +0200

    Added regular draft boosters

--- commands/pack.js
+++ commands/pack.js
@@ -5,6 +5,46 @@ const chance = require('chance').Chance()
 
 // const CUBEDATA = require('../data.js').CUBEDATA
 const scryfallCubes = ['Arena', 'Grixis', 'Legacy', 'Chuck', 'Twisted', 'Protour', 'Uncommon', 'April', 'Modern', 'Amaz', 'Tinkerer', 'Livethedream', 'Chromatic', 'Vintage']
+const premierSets = ['MH2']
+
+const booster = [
+  {
+    multiple: true,
+    options: [
+      {
+        query: 'rarity=rare',
+        chance: 7.4
+      },
+      {
+        query: 'rarity=mythic',
+        chance: 1
+      }
+    ],
+    count: 1
+  },
+  {
+    multiple: false,
+    query: 'watermark=set',
+    count: 1
+  },
+  {
+    multiple: false,
+    query: 'rarity=uncommon',
+    count: 3
+  },
+  {
+    multiple: false,
+    query: 'rarity=common',
+    count: 10
+  }
+]
+
+const ps = {
+  columns: 5,
+  spacing: 10,
+  cardWidth: 2 * 149,
+  cardHeight: 2 * 208
+}
 
 module.exports = {
   name: 'pack',
@@ -16,6 +56,8 @@ module.exports = {
       message.channel.send(`You need to specify which cube you want a pack of. The available cubes are: ${scryfallCubes.join(', ')}. Let me know if you would like your cube to be added!`)
     } else if (scryfallCubes.map(x => x.toLowerCase()).includes(args[0].toLowerCase())) {
       generateCubePack(args[0], message.channel)
+    } else if (premierSets.includes(args[0].toUpperCase())) {
+      generateBoosterPack(args[0], message.channel)
     } else {
       message.channel.send(`This was interpreted as a cubecobra ID, here's your pack: https://cubecobra.com/cube/samplepackimage/${args[0]}/${String(Math.floor(Math.random() * 10000000))}`)
     }
@@ -25,46 +67,79 @@ module.exports = {
   }
 }
 
-async function generateCubePack (cubeName, channel, numberOfCards = 15) {
-  const columns = 5
-  const message = `Here is a ${cubeName} Cube pack, let us know what your first pick would be!`
-  const spacing = 10
-  const cardWidth = 149 * 2
-  const cardHeight = 208 * 2
-  const rows = Math.ceil(numberOfCards / columns)
-  const canvas = Canvas.createCanvas(columns * (cardWidth + spacing) + spacing, rows * (cardHeight + spacing) + spacing)
-  const ctx = canvas.getContext('2d')
+function generateCubePack (cubeName, channel, numberOfCards = 15) {
+  const [canvas, context] = getCanvasAndContext(numberOfCards)
+  let drawn = 0
 
   fetch(`https://api.scryfall.com/cards/search?format=json&include_multilingual=false&q=cube=${cubeName}`)
     .then((response) => response.json())
     .then((result) => {
-      if (result.object !== 'list') {
-        channel.send('Scryfall responded to the request with an error')
-      } else {
-        const indices = chance.unique(chance.natural, numberOfCards, { min: 0, max: result.total_cards - 1 })
-        let drawn = 0
-        for (let i = 1; i < Math.ceil(result.total_cards / 175) + 1; i++) {
-          fetch(`https://api.scryfall.com/cards/search?format=json&include_extras=true&include_multilingual=false&order=name&page=${i}&q=cube=${cubeName}&unique=cards`)
-            .then((response) => response.json())
-            .then((page) => {
-              for (const choice of indices) {
-                if (175 * i > choice && choice > 175 * (i - 1)) {
-                  const card = page.data[choice % 175]
-                  if (card.image_uris === undefined || (Object.keys(card).includes('card_faces') && card.card_faces[0] === undefined)) {
-                    console.log(card.name)
-                  }
-                  Canvas.loadImage(Object.keys(card).includes('card_faces') ? card.card_faces[0].image_uris.png : card.image_uris.png)
-                    .then(image => {
-                      ctx.drawImage(image, spacing + (drawn % columns) * (cardWidth + spacing), spacing + Math.floor(drawn / columns) * (cardHeight + spacing), cardWidth, cardHeight)
-                      drawn++
-                      if (drawn === numberOfCards) {
-                        channel.send(message, new Discord.MessageAttachment(canvas.toBuffer(), 'CubePack.png'))
-                      }
-                    })
-                }
-              }
-            })
+      const indices = chance.unique(chance.natural, numberOfCards, { min: 0, max: result.total_cards - 1 })
+      loadBatch(`cube=${cubeName}`, context, [...Array(numberOfCards).keys()], indices, function () {
+        drawn++
+        if (drawn === numberOfCards) {
+          channel.send(`Here is a pack of ${cubeName} Cube!`, new Discord.MessageAttachment(canvas.toBuffer(), 'CubePack.png'))
         }
-      }
+      })
     })
+}
+
+function generateBoosterPack (set, channel) {
+  const numberOfCards = Object.entries(booster).reduce((accu, value) => accu + value.count, 0)
+  const [canvas, context] = getCanvasAndContext(numberOfCards)
+  const spaces = [...Array(numberOfCards).keys()]
+  let drawn = 0
+
+  for (const slot of booster) {
+    const query = slot.multiple ? chance.weighted(slot.options.map(x => x.query), slot.options.map(x => x.weight)) : slot.query
+    fetch(`https://api.scryfall.com/cards/search?format=json&include_multilingual=false&q=set=${set} ${query}`)
+      .then((response) => response.json())
+      .then((result) => {
+        const indices = chance.unique(chance.natural, slot.count, { min: 0, max: result.total_cards - 1 })
+        loadBatch(`set=${set} ${query}`, context, spaces.splice(0, slot.count), indices, function () {
+          drawn++
+          if (drawn === numberOfCards) {
+            channel.send(`Here is a pack of ${set}!`, new Discord.MessageAttachment(canvas.toBuffer(), 'CubePack.png'))
+          }
+        })
+      })
+  }
+}
+
+function loadBatch (query, context, spaces, indices, callback) {
+  for (let i = 1; i < Math.ceil(Math.max(...indices) / 175) + 1; i++) {
+    fetch(`https://api.scryfall.com/cards/search?format=json&include_extras=true&include_multilingual=false&order=name&page=${i}&q=${query}&unique=cards`)
+      .then((response) => response.json())
+      .then((page) => {
+        for (const choice of indices) {
+          if (175 * i > choice && choice > 175 * (i - 1)) {
+            const card = page.data[choice % 175]
+            Canvas.loadImage(Object.keys(card).includes('card_faces') ? card.card_faces[0].image_uris.png : card.image_uris.png)
+              .then(image => {
+                drawCard(context, image, spaces.shift())
+              })
+              .catch(reason => {
+                console.log(`Error with loading the image of ${card.name}. Reason: ${reason}`)
+              })
+              .finally(() => callback())
+          }
+        }
+      })
+  }
+}
+
+function drawCard (context, image, slot) {
+  context.drawImage(...[
+    image,
+    ps.spacing + (slot % ps.columns) * (ps.cardWidth + ps.spacing),
+    ps.spacing + Math.floor(slot / ps.columns) * (ps.cardHeight + ps.spacing),
+    ps.cardWidth,
+    ps.cardHeight
+  ])
+}
+
+function getCanvasAndContext (numberOfCards) {
+  const rows = Math.ceil(numberOfCards / ps.columns)
+  const canvas = Canvas.createCanvas(ps.columns * (ps.cardWidth + ps.spacing) + ps.spacing, rows * (ps.cardHeight + ps.spacing) + ps.spacing)
+  return [canvas, canvas.getContext('2d')]
 }
