0b7499496bac787a33595dda6771af8296b601d4
PonderKoKo <89521080+PonderKoKo@users.noreply.github.com>
2021-10-16 14:20:37 +0200

    Update intro.js

--- commands/intro.js
+++ commands/intro.js
@@ -6,6 +6,6 @@ module.exports = {
 	description: 'Gives a short introduction of this bot',
 	examples: ['intro'],
 	execute(message) {
-		message.channel.send(`This bot was entirely written by @KoKonuts. It is intended to automatically provide spoilers for the newest MtG sets. To do so it fetches Scryfall-Data in certain intervals and sends any new cards it finds. By default this bot does not do anything. Type ${prefix}howtoadd for instructions on how to add this bot to your server and ${prefix}help for info on the commands you can use. Me and this project are NOT endorsed by Scryfall.`);
+		message.channel.send(`This bot was entirely written by @KoKonuts. It automatically posts spoilers to configured channels and can also be used to generate Pack 1 Pick 1 images of cubes. By default this bot does not do anything. Type ${prefix}howtoadd for instructions on how to add this bot to your server and ${prefix}help for info on the commands you can use. Me and this project are NOT endorsed by Scryfall. You can view the code on github.com/PonderKoKo/KoKoBot or buy me a ko-fi at Ko-fi.com/kokonuts.`);
 	},
 };
